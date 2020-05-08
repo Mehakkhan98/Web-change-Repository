d5169bd5d63560f97978b18ffccc270bf3e882ef
Mehak <52915627+Mehakkhan98@users.noreply.github.com>
2020-05-08 22:11:00 +0500

    Start Of Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import Route from '../src/Router/Routes';
+export default class App extends  React.Component{
+render()
+{
+    return(
+        
+        <Route/>
+       
+    )
+}
+}
+ 
